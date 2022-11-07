abc03c7407027b433fd13d209eeb1a9a2caa161e
Tazzmanian <banskaliqtabg@gmail.com>
2022-11-07 15:19:53 +0200

    Add header

--- crwn-app/src/App.js
+++ crwn-app/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes, Switch } from 'react-router-dom';
 import './App.css';
+import { Header } from './components/header/header.component';
 
 import { HomePage  } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -7,6 +8,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage/>} />
