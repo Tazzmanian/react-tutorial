f05e12034e73349966fb30a9caa2eedfd7210878
Tazzmanian <banskaliqtabg@gmail.com>
2022-11-07 13:24:47 +0200

    shop page

--- crwn-app/src/App.js
+++ crwn-app/src/App.js
@@ -2,19 +2,14 @@ import { Route, Routes, Switch } from 'react-router-dom';
 import './App.css';
 
 import { HomePage  } from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/hats' element={<HatsPage/>} />
+        <Route path='/shop' element={<ShopPage/>} />
       </Routes>
     </div>
   );
