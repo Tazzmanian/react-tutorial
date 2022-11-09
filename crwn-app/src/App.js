b11b529054e7860fb22a54afaae02628bf8d2cfa
Tazzmanian <banskaliqtabg@gmail.com>
2022-11-09 11:56:44 +0200

    Store the subscription and unsubscribe

--- crwn-app/src/App.js
+++ crwn-app/src/App.js
@@ -1,22 +1,48 @@
+import React from 'react';
 import { Route, Routes, Switch } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 import { HomePage  } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { SignPage } from './pages/sign/sign.component';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/shop' element={<ShopPage/>} />
-        <Route path='/sign-in' element={<SignPage/>} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      console.log(user)
+      this.setState({currentUser: user});
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
+  render() {
+    return (
+      <div>
+        <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/shop' element={<ShopPage/>} />
+          <Route path='/sign-in' element={<SignPage/>} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
