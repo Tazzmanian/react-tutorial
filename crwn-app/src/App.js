76c65c29b98ef1c0fba4cd4da1f44a82b30e1601
Tazzmanian <banskaliqtabg@gmail.com>
2022-11-12 01:39:50 +0200

    saving new users to DB

--- crwn-app/src/App.js
+++ crwn-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes, Switch } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import { HomePage  } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -21,9 +21,10 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
       console.log(user)
-      this.setState({currentUser: user});
+      // this.setState({currentUser: user});
+      createUserProfileDocument(user);
     })
   }
 
--- crwn-app/src/firebase/firebase.utils.js
+++ crwn-app/src/firebase/firebase.utils.js
@@ -21,4 +21,32 @@ const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) {
+        return;
+    }
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    console.log('TTT', snapShot.exists, userAuth.uid);
+
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (err) {
+            console.log('error creating user', err.message)
+        }
+    }
+
+    return userRef;
+};
+
 export default firebase;
